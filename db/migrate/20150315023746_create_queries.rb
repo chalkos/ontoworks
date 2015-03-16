14c82f5ec738f668ee871dfb6b14049e6d84961e
Bruno Ferreira <chalkos@chalkos.net>
2015-03-16 19:50:04 +0000

    specified query limits and null. added belongs_to ontology

--- db/migrate/20150315023746_create_queries.rb
+++ db/migrate/20150315023746_create_queries.rb
@@ -1,8 +1,10 @@
 class CreateQueries < ActiveRecord::Migration
   def change
     create_table :queries do |t|
-      t.string :name
-      t.text :content
+      t.string :name, limit: 255, null: false
+      t.text :content, limit: 1048576, null: false #query maximum size is 1MB
+
+      t.belongs_to :ontology, null: false
 
       t.timestamps null: false
     end
