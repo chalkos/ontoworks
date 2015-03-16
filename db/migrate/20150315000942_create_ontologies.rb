661d118a89d84add87331321596dfb12f8d8fe15
Bruno Ferreira <chalkos@chalkos.net>
2015-03-16 21:06:04 +0000

    added fields to ontology, generated schema

--- db/migrate/20150315000942_create_ontologies.rb
+++ db/migrate/20150315000942_create_ontologies.rb
@@ -1,8 +1,11 @@
 class CreateOntologies < ActiveRecord::Migration
   def change
     create_table :ontologies do |t|
-      t.string :name
-      t.string :directory
+      t.string :name, limit: 255, null: false
+      t.string :link, limit: 255, null: false
+      t.boolean :unlisted, default: false, null: false
+      t.boolean :extendable, default: false, null: false
+      t.datetime :expires, null: false
 
       t.timestamps null: false
     end
