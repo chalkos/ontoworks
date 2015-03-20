2b6005a3fbc70768c1688d1da10f209ae34f7321
Miguel Pinto <mcpinto98@gmail.com>
2015-03-20 01:44:27 +0000

    lixo

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -87,50 +87,6 @@ class QueriesController < ApplicationController
     end
   end
 
-
-    def resultset_to_hash(resultset)
-  meta = resultset.meta_data
-  rows = []
-
-  while resultset.next
-    row = {}
-
-    (1..meta.column_count).each do |i|
-      name = meta.column_name i
-      row[name]  =  case meta.column_type(i)
-                    when -6, -5, 5, 4
-                      # TINYINT, BIGINT, INTEGER
-                      resultset.get_int(i).to_i
-                    when 41
-                      # Date
-                      resultset.get_date(i)
-                    when 92
-                      # Time
-                      resultset.get_time(i).to_i
-                    when 93
-                      # Timestamp
-                      resultset.get_timestamp(i)
-                    when 2, 3, 6
-                      # NUMERIC, DECIMAL, FLOAT
-                      case meta.scale(i)
-                      when 0
-                        resultset.get_long(i).to_i
-                      else
-                        BigDecimal.new(resultset.get_string(i).to_s)
-                      end
-                    when 1, -15, -9, 12
-                      # CHAR, NCHAR, NVARCHAR, VARCHAR
-                      resultset.get_string(i).to_s
-                    else
-                      resultset.get_string(i).to_s
-                    end
-    end
-
-    rows << row
-  end
-  rows
-end
-
   # DELETE /queries/1
   # DELETE /queries/1.json
   def destroy
