258bece5b62cf763d8e8a3f8d6ec09619c49e7ba
Bruno Ferreira <chalkos@chalkos.net>
2015-03-26 23:50:08 +0000

    bootstrap-switch não é utilizado no tema em sí. adicionados ficheiros javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,68 @@
 //
 //= require syntax-highlighter-rails/shCore
 //= require syntax-highlighter-rails/shBrushXml
+
+/* Smooth scrolling for anchors */
+$(document).on('click','a.smooth', function(e){
+    e.preventDefault();
+    var $link = $(this);
+    var anchor = $link.attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top
+    }, 1000);
+});
+
+// Syntax Enable
+SyntaxHighlighter.all();
+
+jQuery(document).ready(function () {
+    $('.nav').on('click mousedown mouseup touchstart touchmove', 'a.has_children', function () {
+        if ( $(this).next('ul').hasClass('open_t') && !$(this).parents('ul').hasClass('open_t')) {
+            $('.open_t').removeClass('open_t');
+            return false;
+        }
+        $('.open_t').not($(this).parents('ul')).removeClass('open_t');
+        $(this).next('ul').addClass('open_t');
+        return false;
+    });
+    $(document).on('click', ':not(.has_children, .has_children *)', function() {
+        if( $('.open_t').length > 0 ) {
+            $('.open_t').removeClass('open_t');
+            $('.open_t').parent().removeClass('open');
+            return false;
+        }
+    });
+
+    // hide #back-top first
+    $("#back-top").hide();
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+            return false;
+        });
+    });
+
+});
+
+// WOW Activate
+new WOW().init();
+
+jQuery(document).ready(function() { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({'overflow':'visible'});
+})
+
