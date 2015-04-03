b7685273b3b87bfc416c8da7f1bd3202d3d7f7d3
Miguel Pinto <mcpinto98@gmail.com>
2015-04-03 02:48:43 +0100

    JS to save a query on DB with little flash notice

--- app/assets/javascripts/queries.js
+++ app/assets/javascripts/queries.js
@@ -1,25 +1,35 @@
 $(document).ready(function(){
-  $("a#run_query").click(function(){
-    var code = $(this).data('ontology');
-    var content = $('span#content').text();
+  $("#button_save").click(function(){
+    var name = "<label>Title: </label><input id=\"name\" type=\"text\">";
+    var desc = "<label>Description: </label><input id=\"desc\" type=\"text\">";
+    var notice = "Necessários preencher próximos campos!";
+
+    $("#name_space").append(name);
+    $("#desc_space").append(desc);
+    $("#notice").append(notice);
+
+    $("#button_save").remove();
+    $("#save_space").append("<a>Save</a>")
+  });
+
+  $("#save_space").click(function(){
+    var code = $('#save_space').attr('data');
+
+    var name = $('#name').val();
+    var desc = $('#desc').val()
+    var query = $('#query_content').val();
+    var content = [name,desc,query];
 
     $.ajax({
       type: 'POST',
       url: '/ontologies/'+code+'/queries',
       data: { content: content },
       success: function (data) {
-        alert('asdasda');
+        $("#notice").text("Query Saved!");
+      },
+      fail: function(data) {
+        $("#notice").text("Errors!");
       }
     });
   });
 });
-
-function objToString (obj) {
-    var str = '';
-    for (var p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            str += p + '::' + obj[p] + '\n';
-        }
-    }
-    return str;
-}
