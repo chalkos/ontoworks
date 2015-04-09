31c0d3ef5f4ec65b1a115d5775087939c0cb2822
Bruno Ferreira <chalkos@chalkos.net>
2015-04-09 18:21:11 +0100

    better tables

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -1,2 +1,21 @@
 module QueriesHelper
+  def format_result_cell(cell)
+    # value - the lexical value of the object
+    # (required, full URIs should be used, not qnames)
+    text = cell['value'].to_s || '_error_'
+
+    # type - one of 'uri', 'literal' or 'bnode'
+    # (required and must be lowercase)
+    text.prepend "[#{cell['type']}] " if cell['type']
+
+    # lang - the language of a literal value
+    # (optional but if supplied it must not be empty)
+    text << "@#{cell['lang']}" if cell['lang']
+
+    # datatype - the datatype URI of the
+    # literal value (optional)
+    text << "^^#{cell['datatype']}" if cell['datatype']
+
+    text
+  end
 end
