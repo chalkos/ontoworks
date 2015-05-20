bcb089e7d06b207ad8a543ff8182f278da7b9e37
Bruno Ferreira <chalkos@chalkos.net>
2015-05-20 01:57:35 +0100

    rails setup and postgres setup completed. missing smtp server

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,14 +5,15 @@
 
 if [ "$1" = 'start' ]; then
   source .env
-  ./railsStart "$2" "$3" "$4"
+  ./railsStart $2 $3 $4
 elif [ "$1" = 'reset_development' ]; then
-  rake db:drop db:create db:migrate schema:load db:seed assets:clobber
+  rake db:drop db:create db:migrate db:schema:load db:seed assets:clobber
   rm -rf db/tdb/*
 elif [ "$1" = 'reset_production' ]; then
   echo "export SECRET_KEY_BASE=$(rake secret)" > .env
+  echo "export CLASSPATH=/usr/share/java/postgresql-jdbc.jar" >> .env
   source .env
-  rake db:drop db:create db:migrate schema:load assets:precompile
+  rake db:drop db:create db:migrate db:schema:load assets:precompile
   rm -rf db/tdb/*
 elif [ "$1" = 'reset_logs' ]; then
   > log/development.log
