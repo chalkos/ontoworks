c0f7b04377536e6f5615dfc760893f01377e9f7b
Bruno Ferreira <chalkos@chalkos.net>
2015-05-21 19:32:43 +0100

    db:schema:load was redundant with db:migrate; added option to update environment without re-build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,8 @@
 
 # Builtin commands:
 # start [max [heap [min]]] - start rails server
+# update_development - bundle install && rake db:migrate
+# update_production - try to update everything to last version keeping current content intact
 # reset_development - clean and prepare development environment
 #                     (although docker container runs only in production)
 # reset_production - clean and prepare a production environment
@@ -17,15 +19,25 @@ if [ "$1" = 'start' ]; then
   source .env
   ./railsStart $2 $3 $4
 
+elif [ "$1" = 'update_development' ]; then
+  source .env
+  bundle install
+  rake db:migrate
+
+elif [ "$1" = 'update_production' ]; then
+  source .env
+  bundle install
+  rake db:migrate assets:precompile
+
 elif [ "$1" = 'reset_development' ]; then
-  rake db:drop db:create db:migrate db:schema:load db:seed assets:clobber
+  rake db:drop db:create db:migrate db:seed assets:clobber
   rm -rf db/tdb/*
 
 elif [ "$1" = 'reset_production' ]; then
   echo "export SECRET_KEY_BASE=$(rake secret)" > .env
   echo "export CLASSPATH=/usr/share/java/postgresql-jdbc.jar" >> .env
   source .env
-  rake db:drop db:create db:migrate db:schema:load assets:precompile
+  rake db:drop db:create db:migrate assets:precompile
   rm -rf db/tdb/*
 
 elif [ "$1" = 'reset_logs' ]; then
