73625909bbcdc75ba3a4f3de601a91673144d233
Miguel Pinto <mcpinto98@gmail.com>
2015-05-24 02:34:02 +0100

    errors pages with new layout

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module AppJenaTdb
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # TELL RAILS TO USE OUR ROUTES FOR ERROR HANDLING
+    config.exceptions_app = self.routes
   end
 end
