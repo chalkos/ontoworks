4b812ccdf4eff499f4b8def9719c2adcc9d66cd7
Miguel Pinto <miguelpinto@MBP-Miguel.home>
2015-07-18 19:26:06 +0100

    added page about us
    -images from github profile

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
       end
   end
 
+  get "/about" => "home#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
